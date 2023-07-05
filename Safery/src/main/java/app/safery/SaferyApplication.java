3050c1f7361392ab6038c083241733fff65a590c
cmglmsr <mgulumser@my.aci.k12.tr>
2023-07-06 00:14:52 +0300

    init

---
+++ Safery/src/main/java/app/safery/SaferyApplication.java
@@ -0,0 +1,13 @@
+package app.safery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaferyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaferyApplication.class, args);
+    }
+
+}
---
+++ Safery/src/test/java/app/safery/SaferyApplicationTests.java
@@ -0,0 +1,13 @@
+package app.safery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaferyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
